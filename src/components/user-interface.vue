61b78a3fe7f90dd532cf32882adf57862642d6a6
18924669438 <jiangjuntao2022>
2023-06-22 17:17:03 +0800

    用户界面

--- src/components/user-interface.vue
+++ src/components/user-interface.vue
@@ -1,7 +1,7 @@
 <script setup>
 import IndexHeader from "@/components/IndexHeader.vue";
 import IndexHeader1 from "@/components/indexHeader1.vue";
-import IndexFooter from "@/components/IndexFooter.vue";
+import IndexFooter1 from "@/components/indexFooter1.vue";
 // 图标
 import "@/assets/CSS/font-awesome-5.15.2.all.css";
 import "@/assets/CSS/tiktok.css";
@@ -15,18 +15,78 @@ import "@/assets/CSS/style6.css";
 import "@/assets/CSS/style7.css";
 
 import { ref } from 'vue'
-import IndexFooter1 from "@/components/indexFooter1.vue";
-// 用户信息
-const activeIndex = ref('1')
-// const activeIndex2 = ref('1')
-// const handleSelect = (string, string[]) => {
-//   console.log(key, keyPath)}
+const activeIndex = ref('1'); // 默认选中的菜单项
+function handleSelect(index) {
+  activeIndex.value = index;
+}
 
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-08',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-06',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-07',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+]
+
+const input = ref('');
+const password = ref('');
+const confirmPassword = ref('');
 </script>
 
 <template>
   <index-header1></index-header1>
-  <div class="card" style="margin-bottom: 0px">
+  <div class="card" style="margin-bottom: 0px;">
     <div class="section">
       <div class="pic">
         <img src="@/assets/images/Jeddy.jpg" alt="图片加载错误！">
@@ -35,211 +95,122 @@ const activeIndex = ref('1')
       <div class="tag">1556782359@qq.com</div>
     </div>
 
-    <div class="bottom-section">
+    <div class="bottom-section" style="height: 5vh !important;margin-bottom: 0px">
       <div class="social-media">
         <a href="#"><i class="fab fa-facebook"></i></a>
         <a href="#"><i class="fab fa-twitter"></i></a>
         <a href="#"><i class="fab fa-instagram"></i></a>
       </div>
-<!--      <div class="videos">190<span>视频</span></div>-->
-<!--      <div class="border"></div>-->
-<!--      <div class="subscribers">240k<span>订阅</span></div>-->
-<!--      <div class="border"></div>-->
-<!--      <div class="videws">7.3M<span>图片</span></div>-->
     </div>
   </div>
 
-<!--用户信息-->
-  <div style="margin-bottom: 50px">
+  <!--用户信息-->
+  <div style="margin-bottom: 50px;">
     <el-menu
         :default-active="activeIndex"
         class="el-menu-demo"
         mode="horizontal"
         @select="handleSelect"
     >
-      <el-menu-item index="1">Account Information</el-menu-item>
-      <el-menu-item index="2">Order Information</el-menu-item>
-      <el-menu-item index="3">Alter Password</el-menu-item>
+      <el-menu-item index="1" @click="handleSelect('1')">Account Information</el-menu-item>
+      <el-menu-item index="2" @click="handleSelect('2')">Order Information</el-menu-item>
+      <el-menu-item index="3" @click="handleSelect('3')">Alter Password</el-menu-item>
     </el-menu>
-  </div>
+    <!--    账户信息-->
+    <div v-show="activeIndex === '1'" >
+      <el-descriptions class="margin-top" :column="1" :size="size" border>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              <el-icon :style="iconStyle">
+                <user/>
+              </el-icon>
+              Username
+            </div>
+          </template>
+          Jeddy
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              <el-icon :style="iconStyle">
+                <iphone/>
+              </el-icon>
+              Email
+            </div>
+          </template>
+          1556782359@qq.com
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              <el-icon :style="iconStyle">
+                <location/>
+              </el-icon>
+              Telephone
+            </div>
+          </template>
+          18924669438
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              <el-icon :style="iconStyle">
+                <tickets/>
+              </el-icon>
+              Membership level
+            </div>
+          </template>
+          <el-tag size="small">first</el-tag>
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              <el-icon :style="iconStyle">
+                <office-building/>
+              </el-icon>
+              Address
+            </div>
+          </template>
+          No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu Province
+        </el-descriptions-item>
+      </el-descriptions>
+    </div>
 
-<!--  <div class="dashboard-list">-->
-<!--    <h3>Profile Details</h3>-->
-<!--    <div class="dashboard-message contact-2 bdr clearfix">-->
-<!--      <div class="row">-->
-<!--        <div class="col-lg-3 col-md-3">-->
-<!--          &lt;!&ndash; Edit profile photo &ndash;&gt;-->
-<!--          <div class="edit-profile-photo">-->
-<!--            <img src="img/avatar/avatar-6.jpg" alt="profile-photo" class="img-fluid">-->
-<!--            <div class="change-photo-btn">-->
-<!--              <div class="photoUpload">-->
-<!--                <span><i class="fa fa-upload"></i></span>-->
-<!--                <input type="file" class="upload">-->
-<!--              </div>-->
-<!--            </div>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--        <div class="col-lg-9 col-md-9">-->
-<!--          <form action="#" method="GET" enctype="multipart/form-data">-->
-<!--            <div class="row">-->
-<!--              <div class="col-lg-6 col-md-6">-->
-<!--                <div class="form-group name">-->
-<!--                  <label>Your Name</label>-->
-<!--                  <input type="text" name="name" class="form-control" placeholder="John Deo">-->
-<!--                </div>-->
-<!--              </div>-->
-<!--              <div class="col-lg-6 col-md-6">-->
-<!--                <div class="form-group email">-->
-<!--                  <label>Your Title</label>-->
-<!--                  <input type="text" name="title" class="form-control" placeholder="Your Title">-->
-<!--                </div>-->
-<!--              </div>-->
-<!--              <div class="col-lg-6 col-md-6">-->
-<!--                <div class="form-group subject">-->
-<!--                  <label>Phone</label>-->
-<!--                  <input type="text" name="phone" class="form-control" placeholder="Phone">-->
-<!--                </div>-->
-<!--              </div>-->
-<!--              <div class="col-lg-6 col-md-6">-->
-<!--                <div class="form-group number">-->
-<!--                  <label>Email</label>-->
-<!--                  <input type="email" name="email" class="form-control" placeholder="Email">-->
-<!--                </div>-->
-<!--              </div>-->
-<!--              <div class="col-lg-12 col-md-12 col-sm-12">-->
-<!--                <div class="form-group message">-->
-<!--                  <label>Personal info</label>-->
-<!--                  <textarea class="form-control" name="message" placeholder="Personal info"></textarea>-->
-<!--                </div>-->
-<!--              </div>-->
-<!--            </div>-->
-<!--          </form>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </div>-->
+    <!--    订单信息-->
+    <div v-show="activeIndex === '2'">
+      <el-table :data="tableData" style="width: 100%" height="250">
+        <el-table-column fixed prop="date" label="Date" width="150" />
+        <el-table-column prop="name" label="Name" width="120" />
+        <el-table-column prop="state" label="State" width="120" />
+        <el-table-column prop="city" label="City" width="320" />
+        <el-table-column prop="address" label="Address" width="600" />
+        <el-table-column prop="zip" label="Zip" width="120" />
+      </el-table>
 
 
-<!--反转卡片!!-->
-  <div class="wrapper">
-    <div class="cols">
-      <div class="col" ontouchstart="this.classList.toggle('hover');">
-        <div class="container">
-          <div class="front">
-            <div class="inner">
-              <p>Diligord</p>
-              <span>Lorem ipsum</span>
-            </div>
-          </div>
-          <div class="back">
-            <div class="inner">
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
-            </div>
+    </div>
+
+
+    <!--    修改密码-->
+    <div v-show="activeIndex === '3'">
+      <div class="container" style="margin-left: 34vw">
+        <div class="row">
+          <label for="email">Email:</label>
+          <div class="input-wrapper">
+            <el-input v-model="input" id="email" placeholder="Please input email" class="password-input" />
           </div>
         </div>
-      </div>
-      <div class="col" ontouchstart="this.classList.toggle('hover');">
-        <div class="container">
-          <div class="front1">
-            <div class="inner">
-              <p>Rocogged</p>
-              <span>Lorem ipsum</span>
-            </div>
-          </div>
-          <div class="back">
-            <div class="inner">
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
-            </div>
+        <div class="row">
+          <label for="password">Password:</label>
+          <div class="input-wrapper">
+            <el-input v-model="password" id="password" type="password" placeholder="Please input password" show-password class="password-input"/>
           </div>
         </div>
-      </div>
-      <div class="col" ontouchstart="this.classList.toggle('hover');">
-        <div class="container">
-          <div class="front2">
-            <div class="inner">
-              <p>Strizzes</p>
-              <span>Lorem ipsum</span>
-            </div>
-          </div>
-          <div class="back">
-            <div class="inner">
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col" ontouchstart="this.classList.toggle('hover');">
-        <div class="container">
-          <div class="front3">
-            <div class="inner">
-              <p>Clossyo</p>
-              <span>Lorem ipsum</span>
-            </div>
-          </div>
-          <div class="back">
-            <div class="inner">
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col" ontouchstart="this.classList.toggle('hover');">
-        <div class="container">
-          <div class="front4">
-            <div class="inner">
-              <p>Rendann</p>
-              <span>Lorem ipsum</span>
-            </div>
-          </div>
-          <div class="back">
-            <div class="inner">
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col" ontouchstart="this.classList.toggle('hover');">
-        <div class="container">
-          <div class="front5">
-            <div class="inner">
-              <p>Reflupper</p>
-              <span>Lorem ipsum</span>
-            </div>
-          </div>
-          <div class="back">
-            <div class="inner">
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col" ontouchstart="this.classList.toggle('hover');">
-        <div class="container">
-          <div class="front6">
-            <div class="inner">
-              <p>Acirassi</p>
-              <span>Lorem ipsum</span>
-            </div>
-          </div>
-          <div class="back">
-            <div class="inner">
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col" ontouchstart="this.classList.toggle('hover');">
-        <div class="container">
-          <div class="front7" >
-            <div class="inner">
-              <p>Sohanidd</p>
-              <span>Lorem ipsum</span>
-            </div>
-          </div>
-          <div class="back">
-            <div class="inner">
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
-            </div>
+        <div class="row">
+          <label for="confirm-password">Confirm password:</label>
+          <div class="input-wrapper">
+            <el-input v-model="confirmPassword" id="confirm-password" type="password" placeholder="Please confirm your password" show-password class="password-input"/>
           </div>
         </div>
       </div>
@@ -250,14 +221,39 @@ const activeIndex = ref('1')
   <router-view ></router-view>
 </template>
 
-<style>
-  .el-menu-demo .el-menu-item{
-    padding-left: 40px !important;
-    padding-right: 40px !important;/* 设置菜单项的右边距 */
-  }
-  .el-menu-demo{
-    justify-content: center; /* 在水平方向上居中 */
-    align-items: center; /* 在垂直方向上居中 */
-    //display:flexible;
-  }
+<style scoped>
+.el-menu-demo .el-menu-item{
+  padding-left: 40px !important;
+  padding-right: 40px !important;/* 设置菜单项的右边距 */
+}
+.el-menu-demo{
+  justify-content: center; /* 在水平方向上居中 */
+  align-items: center; /* 在垂直方向上居中 */
+//display:flexible;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 5vh auto;
+}
+
+.row {
+  display: flex;
+  align-items: center; /* 修改为 center */
+  margin-bottom: 10px; /* 添加此行，用于调整行之间的间距 */
+}
+
+label {
+  width: 150px;
+  padding-right: 10px;
+}
+
+.input-wrapper {
+  flex: 1; /* 添加此行，使输入框填充剩余空间 */
+}
+
+.password-input {
+  width: 250px;
+}
 </style>
