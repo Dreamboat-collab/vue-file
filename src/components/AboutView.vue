afa971489cb3e33aef567ded6e39b46c3d2fb6b9
hideonshit202 <102145182+hideonshit202@users.noreply.github.com>
2023-06-28 12:01:46 +0800

    attraction修正和interface的冲突

--- src/components/AboutView.vue
+++ src/components/AboutView.vue
@@ -1,8 +1,6 @@
 <script setup>
 import {onMounted, onUnmounted} from "vue";
 
-
-
 //image
 import bg0 from "@/assets/img/bg.jpg";
 
@@ -47,13 +45,12 @@ const toLogin=()=>{
 const alertNum=()=>{
   ElMessageBox.alert('+86 114 5141 9198', 'Our common WeChat account is:', {
     confirmButtonText: 'Confirm',
-    type: 'success',
+    type: 'success'
   })
   setTimeout(() => {
     ElMessageBox.close();
   }, 5000);
 }
-
 
 
 
